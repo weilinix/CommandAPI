ae0a076a7bccd18215a3d815c5bf8da79b4242a6
unknown <lw@LWPC.JHYJYDC.COM>
2020-12-19 09:11:43 +0800

    Added PostgreSql connection services

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -7,17 +7,28 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 using CommandAPI.Data;
 
 namespace CommandAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        // Using Dependency Injection to Add the Configuration API
+        public Startup(IConfiguration configuration) 
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            // Registering our DB Context with our Services Container
+            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(Configuration.GetConnectionString("PostgreSqlConnection")));
             // Registers services to enable the use of "Controllers" throughout our application
             services.AddControllers();
             // Applying Dependency Injection
